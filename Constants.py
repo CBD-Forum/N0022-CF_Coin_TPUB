70d641044fb2c3f89014057910ebfd47e39e33ae
zjj <zjj@10.109.32.5>
2017-05-07 20:47:50 +0800

    修改页面bug,去掉。encode('utf8');修改页面hash值显示问题

--- Constants.py
+++ Constants.py
@@ -10,4 +10,4 @@ LOCK_TIME=0
 SEQUENCE=0xFFFFFFFF
 MINNER_PUBADDR='17ZaizPFAB76bVXXDZNMHunzeoFjrwGtS2'
 ZERO_HASH = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-DB_PATH='C:/Users/Administrator/workspace/python/test/crowd_funding_coin/dao/test.db'
+DB_PATH='D:/test.db'
