d4dd0f803ff7a0a12d4f6542bcc757086d343525
zjj <zjj@10.109.32.5>
2017-05-07 16:36:12 +0800

    修改页面结构。原index改为blocks；index后期会添加一个静态也米娜；导航条修改为中文

--- www/app/views.py
+++ www/app/views.py
@@ -6,10 +6,14 @@ from www.app import datass,datas
 from www.app import app
 
 @app.route('/')
+
 @app.route('/index')
 def index():
+    return render_template("index.html")
+@app.route('/blocks')
+def blocks():
     blocks = datass.get_blocks()
-    return render_template("index2.html", blocks = blocks)
+    return render_template("blocks.html", blocks = blocks)
 	
 @app.route('/block')
 #display a block's details by hash
@@ -28,6 +32,7 @@ def wallet():
 #     participated_projects = get_i_participated()
 #     
     return render_template("wallet.html", my_keys = my_keys, my_txs = my_txs) #, my_utxos = my_utxos, launched_projects = launched_projects, participated_projects = participated_projects)
+
  
 @app.route('/indextest')
 def indextest():
