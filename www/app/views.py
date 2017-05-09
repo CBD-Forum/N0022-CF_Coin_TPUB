f9010d5c3cf5a345f3a6880b5ca267dd7265403d
zjj <zjj@10.109.32.5>
2017-05-09 17:54:23 +0800

    修改钱包页面，‘交易管理’改成‘我的utxo'

--- www/app/views.py
+++ www/app/views.py
@@ -26,12 +26,12 @@ def block():
 #display personal infomation
 def wallet():
     my_keys = datass.get_keys()
-    my_txs = datass.get_my_txs()
+    my_utxos = datass.get_my_unused_out_txs()
 #     my_utxos = get_my_utxos()
 #     launched_projects = get_i_launched()
 #     participated_projects = get_i_participated()
 #     
-    return render_template("wallet.html", my_keys = my_keys, my_txs = my_txs) #, my_utxos = my_utxos, launched_projects = launched_projects, participated_projects = participated_projects)
+    return render_template("wallet.html", my_keys = my_keys, my_utxos = my_utxos) #, my_utxos = my_utxos, launched_projects = launched_projects, participated_projects = participated_projects)
 
  
 @app.route('/indextest')
